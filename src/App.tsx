63cd73b0dd9d0484679e1edcfb296b3b81625739
Vera Petrosyan <petrosyan.vera.2002@gmail.com>
2024-03-07 14:54:15 +0300

    ✨ Create table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,57 @@
-function App() {
-  return <>Table</>;
-}
+import { useState } from "react";
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
-export default App;
+import "./index.css";
+import { banks } from "./data/Banks";
+import { columns } from "./components/Columns";
+
+export function App() {
+  const [data] = useState(() => [...banks]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  className={Number(row.id) % 2 ? "td-even" : ""}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
